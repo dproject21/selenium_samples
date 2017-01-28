6391c5f0e375397eacb3fdcc49e88d350e750c20
Daiki Tanoguchi <sunsetocean85@gmail.com>
2017-01-28 17:02:29 +0900

    java sample initial commit

---
+++ java/selenium-sample/src/main/java/net/dproject21/sample/selenium/App.java
@@ -0,0 +1,13 @@
+package net.dproject21.sample.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
