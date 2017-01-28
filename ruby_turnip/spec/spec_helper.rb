021ddce66f66d7c35526827d47d07bdfeaaf1c08
Daiki Tanoguchi <sunsetocean85@gmail.com>
2017-01-28 19:17:59 +0900

    remove unnecessary code

--- ruby_turnip/spec/spec_helper.rb
+++ ruby_turnip/spec/spec_helper.rb
@@ -1,21 +1,13 @@
-require 'rubygems'
-require 'bundler/setup'
 require 'turnip'
 require 'turnip/rspec'
 require 'selenium-webdriver'
 
 Dir.glob("spec/steps/**/*steps.rb") { |f| load f, true }
 
-
-
-
 RSpec.configure do |config|
   config.before(:each) {
     # exec browser
     @driver = Selenium::WebDriver.for :chrome
-
-    # access google
-    #@driver.navigate.to "http://google.com"
   }
 
   config.after(:each) {
